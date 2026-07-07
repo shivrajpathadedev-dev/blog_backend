503d61cd5b462bc42cc8475491ccc4526e8c9bc7
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-07-07 15:59:19 +0530

    ok

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cros({
-    origin: `http://127.0.0.1:5500`,
+    origin: [`http://127.0.0.1:5500`, 'https://frontend-seven-delta-85.vercel.app'],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 }))
 
