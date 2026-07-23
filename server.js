8600bb3d362947fa3b42b8cddbfb979e83fee05a
shivrajpathadedev-dev <shivrajpathade.dev@gmail.com>
2026-07-23 21:00:59 +0530

    ok

--- server.js
+++ server.js
@@ -46,10 +46,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cros({
-    origin: [`http://127.0.0.1:5500`,  'https://frontend-seven-delta-85.vercel.app','https://blog-backend-bbg2.onrender.com','https://http-blogs.vercel.app','http://localhost:4200'],
+    origin: [`http://127.0.0.1:5500`, 'https://http-blog2.vercel.app', 'https://frontend-seven-delta-85.vercel.app','https://blog-backend-bbg2.onrender.com','https://http-blogs.vercel.app','http://localhost:4200'],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 }))
-
     
 //GET API
 app.get('/blogs', (req, res) => {
